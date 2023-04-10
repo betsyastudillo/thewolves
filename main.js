a630b2b4e90c82472847a245bd79d4241036f1a4
Betsy Astudillo <betsymalelyastudillo@hotmail.com>
2023-04-10 15:43:47 -0500

    Agregué la vista de orden al icono de carrito en la barra de navegación y arreglamos vistas de esos menú

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuHambIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
-const hambMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const orderCarrito = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-hambMenu.addEventListener('click', toggleMobileMenu);
+menuHambIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleOrderCarrito);
 
 function toggleDesktopMenu() {
+  const isOrderCarritoClosed = orderCarrito.classList.contains('inactive');
+
+  if (!isOrderCarritoClosed) {
+    orderCarrito.classList.add('inactive');
+  }
   desktopMenu.classList.toggle('inactive');
 }
-
 function toggleMobileMenu() {
+  const isOrderCarritoClosed = orderCarrito.classList.contains('inactive');
+
+  if (!isOrderCarritoClosed) {
+    orderCarrito.classList.add('inactive');
+  }
   mobileMenu.classList.toggle('inactive');
+}
+function toggleOrderCarrito() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+  }
+  else if (!isDesktopMenuClosed) {
+    desktopMenu.classList.add('inactive');
+  }
+
+  orderCarrito.classList.toggle('inactive');
 }
